f3c3cc10a0f999bb14ac04afe8bd578c12997858
Sota Yamashita <sota.yamashita@gmail.com>
2016-05-09 10:40:02 +0900

    Mon  9 May 2016 10:40:02 JST

--- index.js
+++ index.js
@@ -4,8 +4,52 @@ var fs = require("fs");
 const style = fs.readFileSync(`${__dirname}/dest/main.css`, "utf8");
 const root = postcss.parse(style);
 
+const ANNOTATION = '@'.charCodeAt(0);
+const ASTERISK   = '*'.charCodeAt(0);
+const NEWLINE    = '\n'.charCodeAt(0);
+const RE_AT_END  = /[ \n\t\r\f\{\(\)'"\\;/]/g;
+
 root.nodes.map(node => {
   if (node.type === 'comment') {
-    //
+
+    let tokens   = [];
+    let css      = node.text.valueOf();
+    let length   = css.length;
+    let offset   = -1;
+    let line     =  1;
+    let position =  0;
+
+    while ( pos < length ) {
+      code = css.charCodeAt(pos);
+      switch (code) {
+        case ANNOTATION:
+
+          break;
+        default:
+
+      }
+
+      switch (code) {
+        case ANNOTATION:
+            RE_AT_END.lastIndex = pos + 1;
+            RE_AT_END.test(css);
+            if ( RE_AT_END.lastIndex === 0 ) {
+                next = css.length - 1;
+            } else {
+                next = RE_AT_END.lastIndex - 2;
+            }
+            tokens.push(['annotation', css.slice(pos, next + 1),
+                line, pos  - offset,
+                line, next - offset
+            ]);
+            pos = next;
+          break;
+        default:
+      }
+
+      pos++;
+    }
+
+    console.log(tokens);
   }
 })
---
+++ test.js
@@ -0,0 +1,6 @@
+// valueOf() として時の処理内容は？
+console.log("Lorem \nipsum \ndolor \nsit \namet");
+console.log("Lorem \nipsum \ndolor \nsit \namet".valueOf());
+
+// 文字列の n 番目の文字の文字コードを返す
+charCodeAt(n)
